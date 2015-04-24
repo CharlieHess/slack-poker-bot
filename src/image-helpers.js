f3ffebb07ee4d3af5caa4c739e5b6578946af683
Charlie Hess <charlie@slack-corp.com>
2015-04-23 23:24:02 -0700

    Imagemagick is garbage and won't even install JPEG dependencies. Let's work with a library that is dependency-free and Does Smart Things. Enter lwip (light-weight image processor)

--- src/image-helpers.js
+++ src/image-helpers.js
@@ -1,28 +1,56 @@
 const rx = require('rx');
-const im = require('imagemagick');
+const lwip = require('lwip');
 
 class ImageHelpers {
-  
-  static concatenateImages(imagePaths, outputFile, margin=4) {
-    return rx.Observable.create((subj) => {
-      let callback = (err, stdout, stderr) => {
-        if (err) {
-          console.log(stderr);
-          subj.onError(err);
-        } else {
-          console.log(stdout);
-          subj.onNext();
-          subj.onCompleted();
-        }
-      };
+  // Public: Combines two images in a single row, and writes the result to the
+  // output file.
+  //
+  // first - The path to the first image
+  // second - The path to the second image
+  // outputFile - The output file path
+  //
+  // Returns an {Observable} that indicates completion or failure.
+  static concatenateImages(first, second, outputFile) {
+    let subj = new rx.Subject();
+    
+    lwip.open(first, (err, firstImage) => {
+      if (err) subj.onError(err);
+      
+      let originalWidth = firstImage.width();
+      let originalHeight = firstImage.height();
+      
+      lwip.open(second, (err, secondImage) => {
+        if (err) subj.onError(err);
+        
+        let additionalWidth = secondImage.width();
+        
+        // Create a blank image that has enough space for both, with a white 
+        // background.
+        lwip.create(originalWidth + additionalWidth, originalHeight, 'white', (err, blankImage) => {
+          if (err) subj.onError(err);
+          
+          blankImage.paste(0, 0, firstImage, (err, hasFirstImage) => {
+            if (err) subj.onError(err);
 
-      im.montage([imagePaths, 
-        '-mode', 'concatenate',               // Append the images next to each other
-        '-tile', 'x1',                        // Lay them out in a single row
-        '-geometry', `+${margin}+${margin}`,  // Add a margin between each image
-        outputFile],                          // Specify the output file
-        callback);
+            hasFirstImage.paste(originalWidth, 0, secondImage, (err, combinedImage) => {
+              if (err) subj.onError(err);
+              
+              // After both images have been pasted, output the result.
+              combinedImage.writeFile(outputFile, (err) => {
+                if (err) {
+                  subj.onError(err);
+                } else {
+                  subj.onNext(outputFile);
+                  subj.onCompleted();
+                }
+              });
+            });
+          });
+        });
+      });
     });
+    
+    return subj;
   }
 }
 
